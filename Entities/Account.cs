bc51372600ca87f36b08a808eee021f210eaf423
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-12-05 13:51:16 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace AppExercBusinessAccount.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            this.Number = number;
+            this.Holder = holder;
+            this.Balance = balance;
+        }
+
+        public void WithDraw(double amount) 
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount) 
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AppExercBusinessAccount.Entities;
+
+namespace AppExercBusinessAccount
+{
+    /// <summary>
+    /// Exercicio - POO: Herança e Polimorfismo
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Braian Lemos", 100.00,500);
+            Console.WriteLine(account.Balance);
+            Console.ReadLine();
+        }
+    }
+}
