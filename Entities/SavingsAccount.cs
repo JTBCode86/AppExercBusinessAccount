0a7e2c6ef1547419e3b08e2f90ba4289b31daeeb
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-12-05 15:47:07 -0300

    C# - Usando o sealed

--- Entities/SavingsAccount.cs
+++ Entities/SavingsAccount.cs
@@ -17,7 +17,8 @@
             Balance += Balance * InterestRate;
         }
 
-        public override void WithDraw(double amount)
+        //Evitando que o metodo seja herdado (Está selada "sealed")
+        public sealed override void WithDraw(double amount)
         {
             //Usando a palavra base. Uso a regra da classe base e implemento também um nova regra:
             //Desconto de 2.0
